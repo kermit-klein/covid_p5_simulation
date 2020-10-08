29ac1aa7a2e8a0c779b625f6669e0a089343d425
allezz <aerbay@gmail.com>
2020-10-09 01:21:15 +0200

    Add constants

--- src/modules/sketch.js
+++ src/modules/sketch.js
@@ -2,3 +2,9 @@ let homeImg;
 function preload() {
   homeImg = loadImage("../home_icon.png");
 }
+
+const INITIAL_INFECTED_PEOPLE = 1;
+const CONTAMINATION_RADIUS = 1;
+const INCUBATION_PERIOD = 2;
+let SOCIAL_DISTANCING_TIME = 5;
+let numberOfPeople = 1000;
