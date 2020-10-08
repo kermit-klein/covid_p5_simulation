c6cfa1b473b74eba3cef6933872220b2ae684ba3
allezz <aerbay@gmail.com>
2020-10-08 04:43:33 +0200

    Add constructor function to Person class

--- src/modules/person.js
+++ src/modules/person.js
@@ -1,3 +1,16 @@
-class Person {}
+class Person {
+  constructor(isInfected = false) {
+    this.pos = new p5.Vector(
+      random() * (windowWidth - 200) + 200,
+      random() * windowHeight
+    );
+    this.angle = new p5.Vector(random(), random()).normalize();
+    this.infectedAt = null;
+    if (isInfected) {
+      this.infectedAt = 0;
+      this.pos = new p5.Vector(windowWidth / 2, windowHeight / 2);
+    }
+  }
+}
 
 export default Person;
