2d4fe95d23a10f64888fbc473f89a0fbfacc3ca7
allezz <aerbay@gmail.com>
2020-10-08 04:54:22 +0200

    Add healthy/ill boolean methods

--- src/modules/person.js
+++ src/modules/person.js
@@ -11,6 +11,15 @@ class Person {
       this.pos = new p5.Vector(windowWidth / 2, windowHeight / 2);
     }
   }
+  isInfected() {
+    return this.infectedAt !== null;
+  }
+  isIll() {
+    return this.infectedAt() && this.infectedAt + INCUBATION_PERIOD < time;
+  }
+  isHealthy() {
+    return this.infectedAt === null;
+  }
 }
 
 export default Person;
