eb139b2f95369f90f4451e89afe19ea6048f890a
allezz <aerbay@gmail.com>
2020-10-08 05:19:18 +0200

    Add draw method for person

--- src/modules/person.js
+++ src/modules/person.js
@@ -48,6 +48,36 @@ class Person {
     this.pos.x = min(windowWidth, this.pos.x);
     this.pos.y = min(maxY, this.pos.y);
   }
+  checkIfInfected() {
+    if (this.isInfected()) {
+      return;
+    }
+    persons.forEach((person) => {
+      if (this === person || !person.isInfected()) {
+        return;
+      }
+      if (this.pos.dist(person.pos) < CONTAMINATION_RADIUS) {
+        this.infectedAt = time;
+      }
+    });
+  }
+  draw() {
+    const x = Math.floor(this.pos.x);
+    const y = Math.floor(this.pos.y);
+    noStroke();
+    if (this.isIll()) {
+      fill(color(255, 0, 0, 50));
+      ellipse(x, y, CONTAMINATION_RADIUS);
+      fill(color("red"));
+    } else if (this.isInfected()) {
+      fill(color(255, 165, 0, 50));
+      ellipse(x, y, CONTAMINATION_RADIUS);
+      fill(color(255, 165, 0));
+    } else {
+      fill(color("green"));
+    }
+    ellipse(x, y, 2);
+  }
 }
 
 export default Person;
